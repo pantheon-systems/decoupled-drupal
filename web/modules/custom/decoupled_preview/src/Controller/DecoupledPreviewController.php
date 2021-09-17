7d366941a4cbdd71ee996255f0c68b4b5071de47
Brian Perry <brian.perry@pantheon.io>
2021-09-16 20:01:03 -0500

    DB-531: POC of making node preview available to decoupled preview tab

--- web/modules/custom/decoupled_preview/src/Controller/DecoupledPreviewController.php
+++ web/modules/custom/decoupled_preview/src/Controller/DecoupledPreviewController.php
@@ -15,17 +15,23 @@ class DecoupledPreviewController extends ControllerBase {
    * Builds the response.
    */
   public function build($node) {
-    $alias = \Drupal::service('path_alias.manager')->getAliasByPath('/node/' . $node);
+    $markup = '';
 
+    $is_preview = str_contains($node, '-');
+    if ($is_preview) {
+      $markup .= '<p>PREVIEW DURING EDIT MODE</p>';
+    }
+
+    $alias = \Drupal::service('path_alias.manager')->getAliasByPath('/node/' . $node);
     $storage = \Drupal::entityTypeManager()->getStorage('dp_preview_site');
     $ids = \Drupal::entityQuery('dp_preview_site')->execute();
     $sites = $storage->loadMultiple($ids);
 
     $links = [];
 
-    foreach($sites as $site) {
+    foreach ($sites as $site) {
       $title = $site->label();
-      $url = $site->get('url'); 
+      $url = $site->get('url');
       $secret = $site->get('secret');
 
       $options = [
@@ -41,16 +47,34 @@ class DecoupledPreviewController extends ControllerBase {
     }
 
     $list = '';
-    foreach($links as $link) {
+    foreach ($links as $link) {
       $list .= '<li>' . $link . '</li>';
     }
     if ($list === '') {
       $list = '<li>' . $this->t('No preview sites available.') . '</li>';
     }
 
+    $markup .= '<ul>' . $list . '</ul>';
+
+    if ($is_preview) {
+      $form_state = \Drupal::service('tempstore.private')->get('node_preview')->get($node);
+      $entity = $form_state->getFormObject()->getEntity();
+
+      $body = $entity->get('body')->value;
+      $markup .= "<p>Body: {$body}</p>";
+
+      $serializer = \Drupal::service('serializer');
+      $data = $serializer->serialize(
+        $entity,
+        'json',
+        ['plugin_id' => 'entity']
+      );
+      $markup .= "<p>Serialized Data: {$data}</p>";
+    }
+
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => "<ul>{$list}</ul>",
+      '#markup' => $markup,
     ];
 
     return $build;
