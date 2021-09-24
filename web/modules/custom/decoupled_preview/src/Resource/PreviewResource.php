2e171085af461b923c92299fec1be3ab700926cb
Brian Perry <brian.perry@pantheon.io>
2021-09-24 12:10:53 -0500

    DB-531: Resolve caching issues with preview while editing

--- web/modules/custom/decoupled_preview/src/Resource/PreviewResource.php
+++ web/modules/custom/decoupled_preview/src/Resource/PreviewResource.php
@@ -5,6 +5,9 @@ namespace Drupal\decoupled_preview\Resource;
 use Drupal\jsonapi_resources\Resource\EntityResourceBase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\jsonapi\CacheableResourceResponse;
+use Drupal\Core\Cache\Cache;
 
 /**
  * Processes a request for a collection containing a resource being edited.
@@ -34,9 +37,25 @@ class PreviewResource extends EntityResourceBase {
     $form_state = \Drupal::service('tempstore.shared')->get('decoupled_preview')->get($tempstore_key);
     // Try / catch or better error handling.
     $entity = $form_state->getFormObject()->getEntity();
-    $data = $this->createIndividualDataFromEntity($entity);
+    $nid = $entity->id();
 
+    /**
+     * This is a pretty big hammer, and could make the entity less cachable for
+     * other JSON:API endpoints.
+     * TODO - refine this, ideally making t possible to invalidate using a cache
+     * tag that will only be relevant to preview.
+     */
+    Cache::invalidateTags(["node:{$nid}"]);
+
+    $data = $this->createIndividualDataFromEntity($entity);
     $response = $this->createJsonapiResponse($data, $request);
+
+    // Add cache tag so that we can invalidate when preview data changes.
+    if ($response instanceof CacheableResourceResponse) {
+      $cache_tags[] = "decoupled_preview:{$tempstore_key}";
+      $cacheability = (new CacheableMetadata())->addCacheTags($cache_tags);
+      $response->addCacheableDependency($cacheability);
+    }
 
     return $response;
   }
