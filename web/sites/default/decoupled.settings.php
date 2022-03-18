526e5a4f2bba74c1dc32cbe459d8f8a3af08f2ee
Abhisek Mazumdar <abhisekmajumdar4@gmail.com>
2022-03-18 18:32:06 +0530

    DB-1650: Enable CORS settings only for local.

--- web/sites/default/decoupled.settings.php
+++ web/sites/default/decoupled.settings.php
@@ -123,6 +123,8 @@ $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
 if ($is_local_env) {
   $config['environment_indicator.indicator']['name'] = 'Local';
   $config['environment_indicator.indicator']['bg_color'] = '#3363aa';
+  // Load services definition file specific to Decoupled.
+  $settings['container_yamls'][] = __DIR__ . '/cors.decoupled.services.yml';
 }
 
 if ($is_pantheon_dev_env){
@@ -140,7 +142,3 @@ if ($is_pantheon_prod_env) {
   $config['environment_indicator.indicator']['bg_color'] = '#aa3333';
 }
 
-/**
- * Load services definition file specific to Decoupled.
- */
-$settings['container_yamls'][] = __DIR__ . '/decoupled.services.yml';
