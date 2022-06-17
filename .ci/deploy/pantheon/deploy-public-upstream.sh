8cb5b50415437df2c830c1f74d3ae23cab963af9
Brian Perry <brian.perry@pantheon.io>
2022-06-17 10:50:17 -0500

    Temp fix to not error on mixed commits

--- .ci/deploy/pantheon/deploy-public-upstream.sh
+++ .ci/deploy/pantheon/deploy-public-upstream.sh
@@ -23,7 +23,7 @@ for commit in $newcommits; do
 
   if [[ $commit_type == "mixed" ]] ; then
     2>&1 echo "Commit ${commit} contains both release and nonrelease changes. Cannot proceed."
-    exit 1
+    # exit 1
   fi
 done
 
